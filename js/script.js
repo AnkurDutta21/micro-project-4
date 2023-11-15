ee5e7324e6abf4923eaf38bfa485bba166ccd1dd
ankur <ankurdutta@apptunix.com>
2023-11-15 13:16:14 -0800

    correction del function

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
 const buttons = document.querySelectorAll(".btn")
 const display = document.querySelector('.display')
+
+
+
 let defaultValue = '0'
-let newValue = ''
+// let newValue = ''default value
+
+
 display.innerHTML = defaultValue
+
+// taking values from button and useing the button function perform actions
+
+
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         const input = btn.value
         buttonFunc(input)
-        display.innerHTML = newValue
+        if(newValue.length !== 0){
+            display.innerHTML = newValue
+        }
+        else{
+            display.innerHTML = defaultValue
+        }
         console.log(input, 'clicked');
     });
 });
 
 
-// result 
-const buttonFunc= (input)=>{
-    if (input === '=') {
-        try {
-            newValue = eval(newValue)
-        } catch (error) {
-            newValue = 'Error'
-        }
-    } else if (input === 'Reset') {
-        newValue = defaultValue
-    } else if (input === 'Del') {
-        newValue = newValue.slice(0, -1)
-    } else {
-        newValue += input
+// button function 
+const buttonFunc = (input) => {
+    switch (input) {
+        case '=':
+            const result = eval(newValue);
+            newValue = result
+            // console.log(typeof(result))
+            break;
+        case 'Reset':
+            newValue = defaultValue;
+            break;
+        case 'Del':
+            if(newValue.length !== 0){
+                newValue = newValue.toString().slice(0, -1);
+            }
+            else{
+                newValue = ''
+            }
+            break;
+        default:
+            newValue += input;
+            break;
     }
 }
